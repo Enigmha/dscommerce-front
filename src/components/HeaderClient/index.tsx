0a8d2a28e35888d90f1bb52062eb8e1b8367406c
Leandro Cabral <enigmha@hotmail.com>
2023-08-05 23:53:45 -0300

    Links, catalog, product details

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -1,19 +1,27 @@
 import './styles.css';
 import cartIcon from '../../assets/cart.svg';
+import { Link } from 'react-router-dom';
 
 export default function HeaderClient() {
 
     return (
     <header className="dcs-header-client">
       <nav className="dsc-container">
+        <Link to={"/"}>
         <h1>DsCommerce</h1>
+        </Link>
         <div className="dsc-navbar-right">
           <div className="dsc-menu-item-container">
             <div className="dsc-menu-item">
-              <img src={cartIcon} alt="Carrinho de compras" />
+              <Link to={"/cart"}>
+                  <img src={cartIcon} alt="Carrinho de compras" />
+              </Link>
+              
             </div>
           </div>
-          <a href="#">Entrar</a>
+          <Link to={"/login"}>
+            Entrar
+          </Link>
         </div>
       </nav>
     </header>
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,26 +4,22 @@ import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
 import { useParams } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export default function ProductDetails() {
-
   const params = useParams();
 
-  
-  const product = productService.findById (Number(params.productId));
-
+  const product = productService.findById(Number(params.productId));
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {
-          product&&
-        <ProductDetailsCard product={product} />
-        }
+        {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
-          <ButtonInverse text="Inicio" />
+          <Link to={"/"}>
+            <ButtonInverse text="Inicio" />
+          </Link>
         </div>
       </section>
     </main>

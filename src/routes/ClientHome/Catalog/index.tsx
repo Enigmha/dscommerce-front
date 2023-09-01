d899bcf2401e7e669f85ca97e09b450baef214f9
Leandro Cabral <enigmha@hotmail.com>
2023-08-31 23:56:04 -0300

    feito Cath request error

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,22 +3,30 @@ import "./styles.css";
 import SeachBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextPage from "../../../components/ButtonNextPage";
-import * as productService from "../../../services/product-service";
-
-
+import { ProductDTO } from "../../../models/product";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Catalog() {
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products/?size=12").then((response) => {
+      setProducts(response.data.content);
+    });
+  }, []);
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SeachBar />
 
-        <div className="dsc-catalog-cards dsc-md20 dsc-mt20">
-
+        <div className="dsc-catalog-cards dsc-md20 dsc-mt20">          
           {
-            productService.findAll().map(product => <CatalogCard key={product.id} product={product} />)
+            products.map(
+              product => <CatalogCard key={product.id} product={product} />
+            )
           }
-          
         </div>
 
         <ButtonNextPage />
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,19 +2,41 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from "../../../services/product-service";
-import { useParams } from "react-router-dom";
+
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  const navigate = useNavigate();
+
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/products/${params.productId}`)
+      .then(reponse => {
+        console.log(reponse.data);
+        setProduct(reponse.data);
+      })
+      .catch(() =>{
+        navigate("/");
+      });
+  }, []);
+
+  
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {product && <ProductDetailsCard product={product} />}
+        {
+          product &&
+             <ProductDetailsCard product={product} />
+
+        }
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <Link to={"/"}>

c7614bb888963ea51c32f6afc6a4dfd3ff128102
Leandro Cabral <enigmha@hotmail.com>
2024-02-03 17:16:34 -0300

    Context cart count

---
+++ src/components/CartIcon/index.tsx
@@ -0,0 +1,26 @@
+
+import { useContext, useState } from 'react';
+import cartIcon from '../../assets/cart.svg';
+
+
+import "./styles.css";
+import { ContextCartCount } from '../../utils/context-cart';
+
+export default function CartIcon() {
+ 
+    const {contextCartCount } = useContext(ContextCartCount);
+ 
+    return (
+    <>
+      
+        <img src={cartIcon} alt="Carrinho de compras" />
+        {
+            contextCartCount > 0 &&
+            <div className='dsc-cart-count'>{contextCartCount}</div>
+        }
+
+
+      
+    </>
+  );
+}
--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./styles.css";
 import * as cartService from "../../../services/cart-service";
 import { OrderDTO } from "../../../models/order";
 import { Link } from "react-router-dom";
+import { ContextCartCount } from "../../../utils/context-cart";
 
 export default function Cart() {
   const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
 
+  const {setContextCartCount} = useContext(ContextCartCount);
+
   function handleClearClick() {
     cartService.clearCart();
-    setCart(cartService.getCart());
+    const newCart = cartService.getCart();
+    setCart(newCart);
+    setContextCartCount(newCart.items.length);
+    
   }
 
   function handleIncreaseItem(productId: number){
@@ -19,7 +25,9 @@ export default function Cart() {
   
   function handleDecreaseItem(productId: number){
     cartService.decreaseItem(productId);
-    setCart(cartService.getCart());
+    const newCart = cartService.getCart();
+    setCart(newCart);
+    setContextCartCount(newCart.items.length);
   }
 
   return (

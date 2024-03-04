bc5d3c01e1d03f63e86052a86ec5da24d7145fe0
Leandro Cabral <enigmha@hotmail.com>
2024-03-04 01:31:57 -0300

    isAutenticated

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,6 +6,7 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import { ProductDTO } from "../../../models/product";
 import { useEffect, useState } from "react";
 import * as productService from '../../../services/product-service';
+import { isAuthenticated } from "../../../services/auth-service";
 
 type QueryParams ={
   page: number;
@@ -26,6 +27,11 @@ export default function Catalog() {
 
    
   useEffect(() => {
+    console.log("AUTENTICADO", isAuthenticated());
+    
+
+
+    
     productService.findPageRequest(queryParams.page, queryParams.name)
     .then(response => {
       const nextPage = response.data.content;
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,42 +1,61 @@
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
+import { AccessTokenPlayloadDTO, CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET, TOKE_KEY } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
 import * as accessTokenRepository from "../localstorage/acess-token-repository";
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
- 
-    const headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
-    }
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+  };
 
-    const requestBody = QueryString.stringify({...loginData, grant_type: "password" });
+  const requestBody = QueryString.stringify({
+    ...loginData,
+    grant_type: "password",
+  });
 
-    const config : AxiosRequestConfig ={
-        method: "POST",
-        url: "/oauth/token",
-        data: requestBody,
-        headers
-    }
+  const config: AxiosRequestConfig = {
+    method: "POST",
+    url: "/oauth/token",
+    data: requestBody,
+    headers,
+  };
 
-    return requestBackend(config);
-
+  return requestBackend(config);
 }
 
-    export function logout(){
-        localStorage.removeItem(TOKE_KEY);
+export function logout() {
+  localStorage.removeItem(TOKE_KEY);
+}
+
+export function saveAcessToken(token: string) {
+  accessTokenRepository.save(token);
+}
+export function getAcessToken() {
+  return accessTokenRepository.get();
+}
+
+export function getAccessTokenPayload(): AccessTokenPlayloadDTO | undefined {
+  try {
+    const token = accessTokenRepository.get();
+    return token == null
+      ? undefined
+      : (jwtDecode(token) as AccessTokenPlayloadDTO);
+  } catch (error) {
+    return undefined;
+  }
+}
+
+export function isAuthenticated(): boolean {
+    let tokenPayload = getAccessTokenPayload();
+    if(tokenPayload && tokenPayload.exp * 1000 > Date.now()){
+        return true;
     }
-
-    export function saveAcessToken(token: string){
-        accessTokenRepository.save(token);
-    }
-    export function getAcessToken(){
-        return accessTokenRepository.get();
-    }
+    return false;
 
 
-
-    
-
+  
+};

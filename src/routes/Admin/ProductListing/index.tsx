3e633bfa60016a932a0043c8f03fa4ab7ef37dac
Leandro Cabral <enigmha@hotmail.com>
2024-03-22 22:03:47 -0300

    Dialog modal info

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -1,48 +1,62 @@
-import './styles.css';
-import * as productService from '../../../services/product-service';
-import editIcon from '../../../assets/edit.svg';
-import deleteIcon from '../../../assets/delete.svg';
-import { useEffect, useState } from 'react';
-import { ProductDTO } from '../../../models/product';
-import SeachBar from '../../../components/SearchBar';
-import ButtonNextPage from '../../../components/ButtonNextPage';
+import "./styles.css";
+import * as productService from "../../../services/product-service";
+import editIcon from "../../../assets/edit.svg";
+import deleteIcon from "../../../assets/delete.svg";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+import SeachBar from "../../../components/SearchBar";
+import ButtonNextPage from "../../../components/ButtonNextPage";
+import DialoInfo from "../../../components/DialogInfo";
 
-type QueryParams ={
-    page: number;
-    name: string;
-  }
+type QueryParams = {
+  page: number;
+  name: string;
+};
 
 export default function ProductListing() {
+  const [dialogInfoData, setDialogInfoData] = useState({
+    visible: false,
+    message: "Operação com sucesso",
+  });
 
-    const [islastPage, setIsLastPage] = useState(false);
+  const [islastPage, setIsLastPage] = useState(false);
 
-    const [products, setProducts] = useState<ProductDTO[]>([]);
+  const [products, setProducts] = useState<ProductDTO[]>([]);
 
-    const [queryParams, setQueryParam] = useState<QueryParams>({
-        page: 0,
-        name: ""
+  const [queryParams, setQueryParam] = useState<QueryParams>({
+    page: 0,
+    name: "",
+  });
+
+  useEffect(() => {
+    productService
+      .findPageRequest(queryParams.page, queryParams.name)
+      .then((response) => {
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
       });
+  }, [queryParams]);
 
-      useEffect(() => {
-        productService.findPageRequest(queryParams.page, queryParams.name)
-        .then(response => {
-          const nextPage = response.data.content;
-          setProducts(products.concat(nextPage));
-          setIsLastPage(response.data.last);
-        });
-      }, [queryParams]); 
+  function handleSearch(searchText: string) {
+    setProducts([]);
+    setQueryParam({ ...queryParams, page: 0, name: searchText });
+  }
 
-      function handleSearch(searchText: string){
-        setProducts([]);
-        setQueryParam({...queryParams, page: 0, name: searchText});
-      }
+  function handleNextPageClick() {
+    setQueryParam({ ...queryParams, page: queryParams.page + 1 });
+  }
 
-      function handleNextPageClick(){
-        setQueryParam({...queryParams, page: queryParams.page + 1});
-      }
+  function handleDialogClose(){
+    setDialogInfoData({ ...dialogInfoData, visible: false });
+  }
 
-    return(
-        <main>
+  function handleDeleteClick(){
+    setDialogInfoData({ ...dialogInfoData, visible: true });
+  }
+
+  return (
+    <main>
       <section id="product-listing-section" className="dsc-container">
         <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
@@ -52,8 +66,6 @@ export default function ProductListing() {
 
         <SeachBar onSearch={handleSearch} />
 
-
-
         <table className="dsc-table dsc-mb20 dsc-mt20">
           <thead>
             <tr>
@@ -62,33 +74,48 @@ export default function ProductListing() {
               <th className="dsc-tb768">Preço</th>
               <th className="dsc-txt-left">Nome</th>
               <th></th>
-              <th></th>  
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {
-                products.map(product =>(
-                    <tr key={product.id}>
-              <td className="dsc-tb576">{product.id}</td>
-              <td><img className="dsc-product-listing-image" src={product.imgUrl} alt={product.name}/></td>
-              <td className="dsc-tb768">{product.price.toFixed(2)}</td>
-              <td className="dsc-txt-left">{product.name}</td>
-              <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar"/></td>
-              <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
-            </tr>
-                ))
-            }
-            
-            
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td className="dsc-tb576">{product.id}</td>
+                <td>
+                  <img
+                    className="dsc-product-listing-image"
+                    src={product.imgUrl}
+                    alt={product.name}
+                  />
+                </td>
+                <td className="dsc-tb768">{product.price.toFixed(2)}</td>
+                <td className="dsc-txt-left">{product.name}</td>
+                <td>
+                  <img
+                    className="dsc-product-listing-btn"
+                    src={editIcon}
+                    alt="Editar"
+                  />
+                </td>
+                <td>
+                  <img onClick={handleDeleteClick}
+                    className="dsc-product-listing-btn"
+                    src={deleteIcon}
+                    alt="Deletar"
+                  />
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
 
-        {
-            !islastPage && 
-            <ButtonNextPage onNextPage={handleNextPageClick} />
-        }
-        
+        {!islastPage && <ButtonNextPage onNextPage={handleNextPageClick} />}
       </section>
+
+      {
+      dialogInfoData.visible && 
+      <DialoInfo message={dialogInfoData.message} onDialogClose={handleDialogClose}/>
+      }
     </main>
-    )
+  );
 }
